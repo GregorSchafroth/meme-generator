59057d2e6e6fcf015155eec5a1014112d9a123a8
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-12 16:16:37 +0200

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <div className='header-left'>
+        <img className='troll-face' src="https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png" alt="troll face" />
+        <h1 className='header-title'>Meme Generator</h1>
+      </div>
+      <div className='header-right'>
+        <p className='header-info'>React Course - Project 3</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
