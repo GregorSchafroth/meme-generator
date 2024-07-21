1f20044f5310c6125518c871755401e4d3bb682d
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-07-21 18:12:50 +0200

    add fetch for memes

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import Memes from '../assets/memesData'
+import React from "react"
 
-const Meme = () => {
-  
-  const [meme, setMeme] = React.useState({
-    topText: '',
-    bottomText: '',
-    randomImage: ''
-  })
-
-  console.log(meme.topText, meme.bottomText)
-
-  function getMeme() {    
-    setMeme((prevState) => ({
-      ...prevState,
-      randomImage: getRandomElement(Memes.data.memes).url}))
-  }
-
-  function getRandomElement(arr) {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-  }
-
-  function handleChange(event){
-    const {name, value} = event.target
-    setMeme(prevState => {
-      return {
-        ...prevState,
-        [name]: value
-      }
+export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-  }
+    const [allMemes, setAllMemes] = React.useState([])
+    
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+    
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
+    
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
   
   return (
     <main className='meme-container'>
@@ -38,22 +38,20 @@ const Meme = () => {
         <div className='meme-input-fields'>
           <div className='top-text-block'>
             <label className='topText' htmlFor="topText">Top Text</label>
-            <input className='topField' type="text" id="topText" name="topText" placeholder="text" onChange={handleChange} />
+            <input className='topField' type="text" id="topText" name="topText" placeholder="text" value={meme.topText} onChange={handleChange} />
           </div>
           <div className='bottom-text-block'>
             <label className='bottomText' htmlFor="bottomText">Bottom Text</label>
-            <input className='bottomField' type="text" id="bottomText" name="bottomText" placeholder="text" onChange={handleChange} />
+            <input className='bottomField' type="text" id="bottomText" name="bottomText" placeholder="text" value={meme.bottomText} onChange={handleChange} />
           </div>
         </div>
-        <input className='meme-button' type="submit" placeholder="Get a new meme image 🖼️" onClick={getMeme} />
+        <input className='meme-button' type="submit" placeholder="Get a new meme image 🖼️" onClick={getMemeImage} />
     </div>
     <div className='meme'>
       <div className='img-container'><img className='meme-img' src={meme.randomImage} alt="" /></div>
-      <h2 className="meme--text top">{meme.topText}</h2>
-      <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      <h1 className="meme--text top">{meme.topText}</h1>
+      <h1 className="meme--text bottom">{meme.bottomText}</h1>
     </div>
     </main>
   )
 }
-
-export default Meme
